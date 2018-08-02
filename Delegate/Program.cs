54caad94b8f2a9e5b28897236f167a015ff02edf
D <chizhida@vip.qq.com>
2018-08-02 11:30:49 +0800

    personal demo
    follow webdemo coding

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,15 +1,201 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Delegate
 {
     class Program
     {
+        private enum InvokeFunctionType
+        {
+            invoke = 1,         //同步
+            begininvoke1,       //异步委托写法一
+            begininvoke2,       //异步委托写法二
+            callbackfunction,   //异步回调函数
+            threadwait          //多线程等待
+        }
+
+        private static void TestThread(string threadName)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine();
+            Console.WriteLine("thread begin：thread name is:{2},the current thread id is:{0},current time is:{1},",
+                Thread.CurrentThread.ManagedThreadId, DateTime.Now.ToString("HH:mm:ss:fff"), threadName);
+            long sum = 0;
+            for (int i = 0; i < 99999999; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("thread   end：thread name is:{2},the current thread id is:{0},current time is:{1},",
+                Thread.CurrentThread.ManagedThreadId, DateTime.Now.ToString("HH:mm:ss:fff"), threadName);
+        }
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine();
+
+                InvokeFunctionType type = new InvokeFunctionType();
+                string[] arrayType = Enum.GetNames(type.GetType());
+
+                for (int i = 0; i < Enum.GetNames(type.GetType()).Length; i++)
+                {
+                    Console.WriteLine(i + 1 + "." + arrayType[i]);
+                }
+                Console.WriteLine("0.clear the screen");
+                Console.WriteLine("-1.exit");
+                Console.Write("Please input invoke function：");
+
+                int itemp = 0;
+                string select = Console.ReadLine();
+                int.TryParse(select, out itemp);
+
+                switch (itemp)
+                {
+                    case (int)InvokeFunctionType.invoke:
+                        Invoke();
+                        break;
+                    case (int)InvokeFunctionType.begininvoke1:
+                        BeginInvoke1();
+                        break;
+                    case (int)InvokeFunctionType.begininvoke2:
+                        BeginInvoke2();
+                        break;
+                    case (int)InvokeFunctionType.callbackfunction:
+                        AsyncCallbackFunction();
+                        break;
+                    case (int)InvokeFunctionType.threadwait:
+                        ThreadWait();
+                        break;
+                    case 0:
+                        Console.Clear();
+                        break;
+                    case -1: return;
+                    default:
+                        Console.WriteLine("input error,please reselect");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 同步调用(Invoke)
+        /// </summary>
+        private static void Invoke()
+        {
+            Action<string> myFunc = TestThread;
+            for (int i = 0; i < 5; i++)
+            {
+                string name = string.Format("Click{0}", i);
+                myFunc.Invoke(name);
+            }
+        }
+
+        /// <summary>
+        /// 异步调用(BeginInvoke)写法一
+        /// </summary>
+        private static void BeginInvoke1()
+        {
+            //写法一：利用Action<>内置委托，调用的时候赋值
+            Action<string> myFunc = TestThread;
+            for (int i = 0; i < 5; i++)
+            {
+                string name = string.Format("Click{0}", i);
+                myFunc.BeginInvoke(name, null, null);
+            }
+        }
+
+        /// <summary>
+        /// 异步调用(BeginInvoke)写法二
+        /// </summary>
+        private static void BeginInvoke2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                string name = string.Format("Click{0}", i);
+                Action myFunc = () =>
+                {
+                    TestThread(name);
+                };
+                myFunc.BeginInvoke(null, null);
+            }
+        }
+
+        private static void AsyncCallbackFunction()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Console.WriteLine("----------------- button1_Click begin main thread id is：{0}  --------------------------",
+                Thread.CurrentThread.ManagedThreadId);
+
+            Action<string> myFunc = TestThread;
+            IAsyncResult asyncResult = null;
+            //参数说明：前面几个参数都是方法的参数值，倒数第二个为异步调用的回调函数，倒数第一个为传给回调函数的参数
+            for (int i = 0; i < 1; i++)
+            {
+                string name = string.Format("button1_Click{0}", i);
+                asyncResult = myFunc.BeginInvoke(name, t =>
+                {
+                    Console.WriteLine("i am thread id is {0} callback", Thread.CurrentThread.ManagedThreadId);
+                    //用 t.AsyncState 来获取回调传进来的参数
+                    Console.WriteLine("the paramater pass in is：{0}", t.AsyncState);
+
+                    //测试一下异步返回值的结果
+                    Console.WriteLine("async return values result is：{0}", t.Equals(asyncResult));
+                }, "maru");
+            }
+
+            //等待的方式1：会有时间上的误差
+            //while (!asyncResult.IsCompleted)
+            //{
+            //    Console.WriteLine("waiting");
+            //}
+
+            // 等待的方式二:
+            //asyncResult.AsyncWaitHandle.WaitOne();//一直等待
+            //asyncResult.AsyncWaitHandle.WaitOne(-1);//一直等待
+            //asyncResult.AsyncWaitHandle.WaitOne(1000);//等待1000毫秒，超时就不等待了
+
+            //等待的方式三：
+            //myFunc.EndInvoke(asyncResult);
+
+            watch.Stop();
+            Console.WriteLine("----------------- button1_Click end main thread id is：{0} the total time taken is：{1}--------------------------",
+                Thread.CurrentThread.ManagedThreadId, watch.ElapsedMilliseconds);
+
+        }
+
+        private static void ThreadWait()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Console.WriteLine("----------------- button1_Click begin main thread id is：{0}  --------------------------",
+                Thread.CurrentThread.ManagedThreadId);
+
+            List<IAsyncResult> list = new List<IAsyncResult>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                string name = string.Format("button1_Click{0}", i);
+                Action myFunc = () =>
+                {
+                    TestThread(name);
+                };
+                var asyncResult = myFunc.BeginInvoke(null, null);
+                list.Add(asyncResult);
+            }
+
+            //下面是线程等待
+            foreach (var item in list)
+            {
+                item.AsyncWaitHandle.WaitOne(-1);
+            }
+
+            watch.Stop();
+            Console.WriteLine("----------------- button1_Click end main thread id is：{0}  the total time taken is：{1}--------------------------",
+                Thread.CurrentThread.ManagedThreadId, watch.ElapsedMilliseconds);
         }
     }
 }
---
+++ DelegateAndEvent/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEvent
+{
+    public delegate void GreetingDelegate(string name);
+
+    public class GreetingManage
+    {
+        /// <summary>
+        /// 在GreetingMange内部声明委托变量
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public GreetingDelegate gmGreetingDelegate;
+
+        //如果GreetingMange类中定义的委托。使用private的话没有意义，使用public的话可以随意进行
+        //赋值操作，言重破坏了对象的封装性，所以这时就是Event的出场时机了。
+        public event GreetingDelegate MakeGreet;
+
+        public void GreetPeople(string name, GreetingDelegate MarkGreeting)
+        {
+            MarkGreeting(name);
+        }
+
+        public void DelegateGreetPeople(string name)
+        {
+            if (gmGreetingDelegate != null)
+            {
+                gmGreetingDelegate(name);
+            }
+        }
+    }
+
+    class Program
+    {
+        #region 使用枚举判断调用对应的方法
+        public enum Language
+        {
+            Chinese, English
+        }
+
+        private static void EnumGreetPeople(string name, Language lang)
+        {
+            switch (lang)
+            {
+                case Language.Chinese:
+                    ChineseGreeting(name);
+                    break;
+                case Language.English:
+                    EnglishGreeting(name);
+                    break;
+            }
+        }
+        #endregion
+
+        private static void GreetPeopple(string name, GreetingDelegate MakeGreeting)
+        {
+            MakeGreeting(name);
+        }
+
+        private static void EnglishGreeting(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+        private static void ChineseGreeting(string name)
+        {
+            Console.WriteLine("您好 " + name);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("枚举：");
+            EnumGreetPeople("枚举", Language.Chinese);
+            EnumGreetPeople("Enum", Language.English);
+            Console.WriteLine();
+
+            Console.WriteLine("委托：");
+            GreetingDelegate delegate1, delegate2;
+            delegate1 = ChineseGreeting;
+            delegate2 = EnglishGreeting;
+            GreetPeopple("委托", delegate1);
+            GreetPeopple("delegate", delegate2);
+            Console.WriteLine();
+
+            Console.WriteLine("委托绑定一：");
+            GreetingDelegate delegate3;
+            delegate3 = ChineseGreeting;
+            delegate3 += EnglishGreeting;
+            GreetPeopple("bunding", delegate3);
+            Console.WriteLine();
+
+            Console.WriteLine("委托绑定二：");
+            delegate3("bundingtwo");
+            Console.WriteLine();
+
+            Console.WriteLine("委托解绑：");
+            delegate3 -= EnglishGreeting;
+            delegate3("unbunding");
+            Console.WriteLine();
+
+            Console.WriteLine("新类：");
+            GreetingManage gm = new GreetingManage();
+            gm.GreetPeople("gm类", ChineseGreeting);
+            gm.GreetPeople("classgm", EnglishGreeting);
+            Console.WriteLine();
+
+            Console.WriteLine("委托新类：");
+            GreetingManage gm2 = new GreetingManage();
+            GreetingDelegate delegate4;
+            delegate4 = ChineseGreeting;
+            delegate4 += EnglishGreeting;
+            gm2.GreetPeople("gmclasstwo", delegate4);
+            Console.WriteLine();
+
+            Console.WriteLine("新类定义的委托：");
+            GreetingManage gm3 = new GreetingManage();
+            gm3.gmGreetingDelegate = ChineseGreeting;
+            gm3.gmGreetingDelegate += EnglishGreeting;
+            gm3.gmGreetingDelegate("gmdelegate");
+            Console.WriteLine();
+
+            Console.WriteLine("模拟烧水过程：");
+            Console.WriteLine();
+            Heater heater = new Heater();
+            Alarm alarm = new Alarm();
+            Display display = new Display();
+            heater.BoilEvent += alarm.MakeAlert;            //注册方法
+            heater.BoilEvent += (new Alarm()).MakeAlert;    //给匿名对象注册方法
+            heater.BoilEvent += Display.ShowMsg;            //注册静态方法
+            heater.BoilWater();     //烧水，会自动调用注册过对象的方法
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        #region 模拟热水器烧水过程 Observer设计模式
+        /// <summary>
+        /// 热水器
+        /// </summary>
+        public class Heater
+        {
+            /// <summary>
+            /// 水温
+            /// </summary>
+            private int temperatur;
+
+            public delegate void BoilHandler(int temperatur);
+
+            public event BoilHandler BoilEvent;
+
+            /// <summary>
+            /// 烧水
+            /// </summary>
+            public void BoilWater()
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    temperatur = i;
+                    if (temperatur > 95)
+                    {
+                        if (BoilEvent != null)
+                        {
+                            BoilEvent(temperatur);  //调用所有注册对象的方法
+                        }
+                    }
+                }
+            }
+        }
+
+        public class Alarm
+        {
+            /// <summary>
+            /// 发出语音警报
+            /// </summary>
+            /// <param name="param"></param>
+            public void MakeAlert(int param)
+            {
+                Console.WriteLine("Alarm:叮咚，水温已经{0}度了", param);
+            }
+        }
+
+        public class Display
+        {
+            /// <summary>
+            /// 显示水温
+            /// </summary>
+            public static void ShowMsg(int param)
+            {
+                Console.WriteLine("Display:水快开了，当前温度：{0}度。", param);
+            }
+        }
+        #endregion
+    }
+}
